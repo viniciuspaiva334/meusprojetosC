cc2037d5a53e28dccc84c2c498ac94cbfbc34e4b
Vinícius Paiva <97923123+viniciuspaiva334@users.noreply.github.com>
2022-10-19 19:06:13 +0000

    adicionando codigo de vetores

--- .ccls-cache/@home@runner@BaggyPriceyOctal/vetoresaula.c
+++ .ccls-cache/@home@runner@BaggyPriceyOctal/vetoresaula.c
@@ -3,4 +3,10 @@
 int main(void) {
   printf("Hello World\n");
   return 0;
-}
+}echo "# vetoresc" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/viniciuspaiva334/vetoresc.git
+git push -u origin main
